5b1086c81387dabcd6cce757b79f0c8f144570da
Oliolixinfree <offlane@tut.by>
2020-02-25 20:13:48 +0300

    added an event handler to the button 'Add post' and textarea

--- src/components/MyProfile/MyPosts/MyPosts.jsx
+++ src/components/MyProfile/MyPosts/MyPosts.jsx
@@ -7,15 +7,22 @@ const MyPosts = props => {
     <Post message={p.message} likeCount={p.likeCount} />
   ));
 
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
+
   return (
     <div className={s.postsBlock}>
       <h3>My Posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={s.posts}>{postsElements}</div>
