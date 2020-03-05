06855ca347f46737fe3d778cd6e630bb79ea3c6c
Oliolixinfree <offlane@tut.by>
2020-03-05 22:20:05 +0300

    install and use redux, create redux-store.js

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,63 @@
 const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
 const SEND_MESSAGE = "SEND-MESSAGE";
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+  dialogs: [
+    {
+      id: 1,
+      name: "Elena"
+    },
+    {
+      id: 2,
+      name: "Nastya"
+    },
+    {
+      id: 3,
+      name: "Masha"
+    },
+    {
+      id: 4,
+      name: "Katya"
+    },
+    {
+      id: 5,
+      name: "Dasha"
+    },
+    {
+      id: 6,
+      name: "Tanya"
+    }
+  ],
+  messages: [
+    {
+      id: 1,
+      message: "Hi"
+    },
+    {
+      id: 2,
+      message: "Yo"
+    },
+    {
+      id: 3,
+      message: "How to?"
+    },
+    {
+      id: 4,
+      message: "Hi"
+    },
+    {
+      id: 5,
+      message: "Yo"
+    },
+    {
+      id: 6,
+      message: "Yo"
+    }
+  ],
+  newMessageBody: ""
+};
+
+const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_NEW_MESSAGE_BODY:
       state.newMessageBody = action.body;
