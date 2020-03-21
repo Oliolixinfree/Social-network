65e7c425f9e44218da94ad6ed7c73ae708bf6bdb
Oliolixinfree <offlane@tut.by>
2020-03-21 21:26:25 +0300

    code refactoring

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -5,20 +5,21 @@ class ProfileStatus extends React.Component {
     editMode: false
   };
 
-  activateEditMode() {
+  activateEditMode = () => {
+    console.log(this);
     this.setState({ editMode: true });
-  }
+  };
 
-  deactivateEditMode() {
+  deactivateEditMode = () => {
     this.setState({ editMode: false });
-  }
+  };
 
   render() {
     return (
       <div>
         {!this.state.editMode && (
           <div>
-            <span onDoubleClick={this.activateEditMode.bind(this)}>
+            <span onDoubleClick={this.activateEditMode}>
               {this.props.status}
             </span>
           </div>
@@ -27,7 +28,7 @@ class ProfileStatus extends React.Component {
           <div>
             <input
               autoFocus={true}
-              onBlur={this.deactivateEditMode.bind(this)}
+              onBlur={this.deactivateEditMode}
               value={this.props.status}
             />
           </div>
