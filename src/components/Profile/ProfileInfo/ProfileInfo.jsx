1f478dbfe32119459f4d2e9dd23c69edd2a699ca
Oliolixinfree <offlane@tut.by>
2020-03-21 18:50:18 +0300

    settate, local state, status created

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = props => {
   if (!props.profile) {
@@ -15,6 +16,7 @@ const ProfileInfo = props => {
       <div className={s.descriptionBlock}>
         <div>{props.profile.fullName}</div>
         <img src={props.profile.photos.large} />
+        <ProfileStatus status={"че нах????"} />
         <div>About Me: {props.profile.aboutMe}</div>
         <div>
           Contacts:
