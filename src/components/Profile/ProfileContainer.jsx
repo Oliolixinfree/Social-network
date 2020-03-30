04a7d2daf7b87d3a645ad55e05e8e0f2ee196f54
Oliolixinfree <offlane@tut.by>
2020-03-30 18:23:35 +0300

    mapStateToProps (reselect part 2)

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Profile from "./Profile";
 import { connect } from "react-redux";
 import {
-  getUserProfile,
   getStatus,
+  getUserProfile,
   updateStatus
 } from "../../redux/profile-reducer";
 import { withRouter } from "react-router-dom";
@@ -18,34 +18,34 @@ class ProfileContainer extends React.Component {
         this.props.history.push("/login");
       }
     }
-
     this.props.getUserProfile(userId);
-
     this.props.getStatus(userId);
   }
+
   render() {
+    // console.log("RENDER PROFILE");
     return (
-      <div>
-        <Profile
-          {...this.props}
-          profile={this.props.profile}
-          status={this.props.status}
-          updateStatus={this.props.updateStatus}
-        />
-      </div>
+      <Profile
+        {...this.props}
+        profile={this.props.profile}
+        status={this.props.status}
+        updateStatus={this.props.updateStatus}
+      />
     );
   }
 }
 
-let mapStateToProps = state => ({
-  profile: state.profilePage.profile,
-  status: state.profilePage.status,
-  authorizedUserId: state.auth.userId,
-  isAuth: state.auth.isAuth
-});
+let mapStateToProps = state => {
+  //console.log('mapStateToProps PROFILE')
+  return {
+    profile: state.profilePage.profile,
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth
+  };
+};
 
 export default compose(
   connect(mapStateToProps, { getUserProfile, getStatus, updateStatus }),
   withRouter
-  //withAuthRedirect
 )(ProfileContainer);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import store from "./redux/redux-store";
 import React from "react";
@@ -7,6 +6,10 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+
+/*setInterval(() => {
+    store.dispatch({type: "FAKE"})
+}, 1000);*/
 
 ReactDOM.render(
   <BrowserRouter>
@@ -17,7 +20,8 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// API
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
