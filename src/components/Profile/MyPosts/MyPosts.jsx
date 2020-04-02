22e07bb272b8a14958a9aca7b31563e8d0f4ce20
Oliolixinfree <offlane@tut.by>
2020-04-02 20:50:40 +0300

    shouldComponentUpdate, PureComponent, memo

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
-import { reduxForm, Field } from "redux-form";
+import { Field, reduxForm } from "redux-form";
 import {
   maxLengthCreator,
   required
@@ -10,12 +10,35 @@ import { Textarea } from "../../common/FormsControls/FormsControls";
 
 const maxLength10 = maxLengthCreator(10);
 
-const MyPosts = props => {
+let AddNewPostForm = props => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field
+          name="newPostText"
+          component={Textarea}
+          placeholder={"Post message"}
+          validate={[required, maxLength10]}
+        />
+      </div>
+      <div>
+        <button>Add post</button>
+      </div>
+    </form>
+  );
+};
+
+let AddNewPostFormRedux = reduxForm({ form: "ProfileAddNewPostForm" })(
+  AddNewPostForm
+);
+
+const MyPosts = React.memo(props => {
+  console.log("RENDER YO");
   let postsElements = props.posts.map(p => (
-    <Post message={p.message} likeCount={p.likeCount} />
+    <Post message={p.message} likesCount={p.likesCount} />
   ));
 
-  // let newPostElement = React.createRef();
+  let newPostElement = React.createRef();
 
   let onAddPost = values => {
     props.addPost(values.newPostText);
@@ -28,28 +51,6 @@ const MyPosts = props => {
       <div className={s.posts}>{postsElements}</div>
     </div>
   );
-};
-
-const AddNewPostForm = props => {
-  return (
-    <form onSubmit={props.handleSubmit}>
-      <div>
-        <Field
-          component={Textarea}
-          name={"newPostText"}
-          placeholder={"Enter post text"}
-          validate={[required, maxLength10]}
-        />
-      </div>
-      <div>
-        <button>add post</button>
-      </div>
-    </form>
-  );
-};
-
-const AddNewPostFormRedux = reduxForm({ form: "profileAddNewPostForm" })(
-  AddNewPostForm
-);
+});
 
 export default MyPosts;
